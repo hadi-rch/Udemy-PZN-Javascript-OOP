b506c78dbc03f84572443731fdfde388164ae8ee
hadirch <hadirich3@gmail.com>
2023-09-14 08:24:22 +0700

    constructor function

---
+++ constructor-function.js
@@ -0,0 +1,15 @@
+//Membuat Constructor function
+/*
+Sebelum Ecmascript v6, pembuatan class, biasanya menggunakan function. hal ini dikarenakan
+sebenarnya javascript bukanlah bahasa pemrograman yang fokus ke OOP
+Untuk membuat class di javascript lama, kita bisa membuat function
+function ini kita sebut dengan Constructor function
+*/
+//Membuat object dari constructor function
+//setelah kita membuat class, jika kita ingin membuat object dari class tersebut, kita bisa
+//menggunakan kata kunci new, lalu diikuti nama contructor function nya
+function Person() {
+
+}
+const eko = new Person();
+const budy = new Person();
